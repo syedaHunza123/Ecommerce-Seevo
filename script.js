7d909b84591ae32c403c7529a43204c9942be36a
syedaHunza123 <nohasyeda1234@gmail.com>
2024-12-02 03:09:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const signButton = document.getElementById('sign-in');
+
+        // Redirect to the form page when the button is clicked
+        signButton.addEventListener('click', () => {
+            window.location.href = 'form.html'; // Redirect to form.html
+        });
